6e76ca0a8f7057451a5a5fb8ccc561df87f3cdba
diarah-zr <diarahtraore78@gmail.com>
2026-01-08 16:37:48 +0100

    js

--- pure_app.js
+++
@@ -1,4 +0,0 @@
-// pure_app.js (désactivé)
-// Ce fichier a été neutralisé pour éviter les doublons dans le projet.
-// Si vous souhaitez le supprimer définitivement, supprimez ce fichier manuellement.
-
